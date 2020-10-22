7951550f5b2273157d215164439a4c0a39d69eaf
LKosoj <lkosoj@gmail.com>
2020-10-22 14:20:06 +0300

    Update Samovar_ini.h

--- Samovar_ini.h
+++ Samovar_ini.h
@@ -2,6 +2,6 @@
 // Параметры подключения к WIFI
 #define SAMOVAR_HOST "samovar"
 #define SAMOVAR_SSID "ssid"
-#define SAMOVAR_PASSWORD "pawd"
+#define SAMOVAR_PASSWORD "pwd"
 #define SAMOVAR_AUTH "auth-auth"
 //**************************************************************************************************************
