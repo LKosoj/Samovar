6f959c780179a3438ea93bed92fb20ace3527b47
Kosoj <lkosoj@gmail.com>
2021-01-24 22:12:52 +0300

    Revert "Update Samovar_ini.h"
    This reverts commit 374f957eecd4440e8fe3332301677f2f28839347.

--- Samovar_ini.h
+++ Samovar_ini.h
@@ -1,6 +1,7 @@
 // Параметры подключения к WIFI
 #define SAMOVAR_HOST "samovar"
 #define SAMOVAR_SSID "ssid"
+#define SAMOVAR_PASSWORD "pawd"
 #define SAMOVAR_PASSWORD "pwd"
 #define SAMOVAR_AUTH "auth-auth"
 //**************************************************************************************************************
