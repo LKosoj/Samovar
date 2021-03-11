202e8a4c334dbbedee13e6a994ce59b8fe94e4ec
Kosoj <lkosoj@gmail.com>
2021-03-12 01:15:27 +0300

    Update Menu.ino

--- Menu.ino
+++ Menu.ino
@@ -485,10 +485,6 @@ void encoder_getvalue() {
       lcd.init();
     }
 
-    Crt = CurrentTime();
-    StrCrt = Crt.substring(6) + "   " + millis2time();
-    StrCrt.toCharArray(tst, 20);
-
     tcnt ++;
     if (tcnt == 3) {
       tcnt = 0;
