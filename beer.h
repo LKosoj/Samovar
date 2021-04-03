35f38139f13fc0b875fb9935e94a0b20790b7331
Kosoj <lkosoj@gmail.com>
2021-04-03 03:35:07 +0300

    Update beer.h

--- beer.h
+++ beer.h
@@ -1,15 +1,19 @@
 void beer_finish();
 
-void beer_proc(){
+void beer_proc(){  
   return;
 }
 
 void IRAM_ATTR beer_finish(){
+    if (fileToAppend) {
+      fileToAppend.close();
+    }
     Msg = "Beer finished";
 #ifdef SAMOVAR_USE_BLYNK
     //Если используется Blynk - пишем оператору
     Blynk.notify("{DEVICE_NAME} Berr finished");
 #endif
+    set_power(false);
     reset_sensor_counter();
 }
 
