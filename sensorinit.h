8d67d0e932987d5c93175eac3169ce4de80944d6
Kosoj <lkosoj@gmail.com>
2021-04-03 03:45:09 +0300

    Update sensorinit.h

--- sensorinit.h
+++ sensorinit.h
@@ -212,7 +212,7 @@ void sensor_init(void) {
 #ifdef USE_WATERSENSOR
   //Настраиваем датчик потока
   pinMode(WATERSENSOR_PIN, INPUT);
-  digitalWrite(WATERSENSOR_PIN, HIGH);
+  //digitalWrite(WATERSENSOR_PIN, HIGH);
 #endif
 
   //  set_program("H;3;1;1;0;45\nB;5;2;1;0;45\nH;6;3;1;0;45\n");
