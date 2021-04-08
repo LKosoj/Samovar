d020f5ae6766854021822609fd2678711c2e9df2
Kosoj <lkosoj@gmail.com>
2021-04-08 15:48:07 +0300

    Update pumppwm.h

--- pumppwm.h
+++ pumppwm.h
@@ -5,13 +5,13 @@
 #define PUMP_PWM_FREQ 100
 
 ESP32PWM pump_pwm;
-GyverPID regulator(10, 0.05, 0.01, 1000);
+GyverPID pump_regulator(10, 0.05, 0.05, 1000);
 
 void init_pump_pwm(byte pin, int freq) {
   pump_pwm.attachPin(pin, freq, 10);
-  regulator.setDirection(REVERSE);          // направление регулирования (NORMAL/REVERSE). ПО УМОЛЧАНИЮ СТОИТ NORMAL
-  regulator.setLimits(PWM_LOW_VALUE * 10, 1023);           // пределы (ставим для 8 битного ШИМ). ПО УМОЛЧАНИЮ СТОЯТ 0 И 255
-  regulator.setpoint = TARGET_WATER_TEMP;   // сообщаем регулятору температуру, которую он должен поддерживать
+  pump_regulator.setDirection(REVERSE);          // направление регулирования (NORMAL/REVERSE). ПО УМОЛЧАНИЮ СТОИТ NORMAL
+  pump_regulator.setLimits(PWM_LOW_VALUE * 10, 1023);           // пределы (ставим для 8 битного ШИМ). ПО УМОЛЧАНИЮ СТОЯТ 0 И 255
+  pump_regulator.setpoint = TARGET_WATER_TEMP;   // сообщаем регулятору температуру, которую он должен поддерживать
   pump_started = false;
 }
 
@@ -27,7 +27,7 @@ void set_pump_pwm(float duty) {
 }
 
 void set_pump_speed_pid(float temp) {
-  regulator.input = temp;
-  set_pump_pwm(regulator.getResultNow());
+  pump_regulator.input = temp;
+  set_pump_pwm(pump_regulator.getResultNow());
 }
 #endif
