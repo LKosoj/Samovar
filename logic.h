69de86ff695ca81ffbb2a2811c5f1b953c3210fc
Kosoj <lkosoj@gmail.com>
2021-04-21 12:12:31 +0300

    Update logic.h

--- logic.h
+++ logic.h
@@ -25,6 +25,23 @@ void set_pump_speed(float pumpspeed, bool continue_process);
 void set_pump_pwm(float duty);
 void set_pump_speed_pid(float temp);
 
+//Получить подстроку через разделитель
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
 //Получаем объем отбора
 float IRAM_ATTR get_liguid_volume_by_step(int StepCount) {
   static float retval;
