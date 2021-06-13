9e8d5a58613b40f4f077b17c556a2938610abe56
LKosoj <lkosoj@gmail.com>
2021-06-13 21:16:22 +0300

    Update pumppwm.h

--- pumppwm.h
+++ pumppwm.h
@@ -20,12 +20,12 @@ void init_pump_pwm(byte pin, int freq) {
 void set_pump_pwm(float duty) {
   if (!pump_started && duty > 0) {
     wp_count = 0;
-    pump_pwm.write(500);
+    pump_pwm.write(400);
     pump_started = true;
     return;
   }
   if (duty > 0 && wp_count < 10) {
-    pump_pwm.write(500);
+    pump_pwm.write(400);
     wp_count++;
     return;
   }
