fee8f0159b730119962816c26749b42b2ea24cbe
Andrew Kornev <kornev.andrew@google.com>
2021-06-21 00:48:16 +0200

    Add user_config_override_sample.h

--- user_config_override.h
+++ user_config_override.h
@@ -1,4 +1,16 @@
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
+#undef SAMOVAR_USE_POWER
+#undef SAMOVAR_USE_RMVK
+
+#undef SERVO_PIN
+#undef USE_WATER_PUMP
+//#undef WATER_PUMP_PIN
+#define USE_WATER_VALVE HIGH                 //использовать управление клапаном для регулировки напора воды
+
+#undef BLYNK_SAMOVAR_TOOL
+
+#define BLYNK_SAMOVAR_TOOL "zorrodeoro.es"
+
 #endif  // _USER_CONFIG_OVERRIDE_H_
