8edcea2dbc43fc5b4b5896989f74a45d4524a331
LKosoj <lkosoj@gmail.com>
2021-06-21 12:06:18 +0300

    Update user_config_override.h

--- user_config_override.h
+++ user_config_override.h
@@ -3,10 +3,10 @@
 
 /*****************************************************************************************************\
  * USAGE:
- *   To modify the stock configuration without changing the Samovar.h and Samovar_ini.h files:
- *   (1) copy this file to "user_config_override.h" (It will be ignored by Git)
- *   (2) define your own settings below
+ *   To modify the stock configuration without changing the Samovar.h and Samovar_ini.h files
+ *   define your own settings below
  *
  ******************************************************************************************************/
+
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
