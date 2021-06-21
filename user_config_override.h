8a31768350d507e4ec0f4a877d71ff7d27b7705d
LKosoj <lkosoj@gmail.com>
2021-06-21 12:22:32 +0300

    Add files via upload

---
+++ user_config_override.h
@@ -0,0 +1,12 @@
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+/*****************************************************************************************************\
+ * USAGE:
+ *   To modify the stock configuration without changing the Samovar.h and Samovar_ini.h files
+ *   define your own settings below
+ *
+ ******************************************************************************************************/
+s
+
+#endif  // _USER_CONFIG_OVERRIDE_H_
