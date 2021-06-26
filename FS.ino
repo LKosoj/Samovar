6353f1a7bc89758dbeb6e6f93552c2ae650e63a4
LKosoj <lkosoj@gmail.com>
2021-06-26 22:07:07 +0300

    Update FS.ino

--- FS.ino
+++ FS.ino
@@ -1,9 +1,9 @@
-const char* http_username = "admin";
-const char* http_password = "admin";
+const char *http_username = "admin";
+const char *http_password = "admin";
 
 String get_sys_info();
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
   if (type == WS_EVT_CONNECT) {
     Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
     client->printf("Hello Client %u :)", client->id());
@@ -11,11 +11,11 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
   } else if (type == WS_EVT_DISCONNECT) {
     Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
   } else if (type == WS_EVT_ERROR) {
-    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
   } else if (type == WS_EVT_PONG) {
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char*)data : "");
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
   } else if (type == WS_EVT_DATA) {
-    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
     String msg = "";
     if (info->final && info->index == 0 && info->len == len) {
       //the whole message is in a single frame and we got all of it's data
@@ -23,13 +23,13 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
 
       if (info->opcode == WS_TEXT) {
         for (size_t i = 0; i < info->len; i++) {
-          msg += (char) data[i];
+          msg += (char)data[i];
         }
       } else {
         char buff[3];
         for (size_t i = 0; i < info->len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
         }
       }
       Serial.printf("%s\n", msg.c_str());
@@ -50,13 +50,13 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
 
       if (info->opcode == WS_TEXT) {
         for (size_t i = 0; i < len; i++) {
-          msg += (char) data[i];
+          msg += (char)data[i];
         }
       } else {
         char buff[3];
         for (size_t i = 0; i < len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
         }
       }
       Serial.printf("%s\n", msg.c_str());
@@ -91,13 +91,11 @@ String formatBytes(size_t bytes) {
 
 
 // Инициализация FFS
-void FS_init(void)
-{
+void FS_init(void) {
   SPIFFS.begin();
   {
     File dir = SPIFFS.open("/");
-    while (dir.openNextFile())
-    {
+    while (dir.openNextFile()) {
       String fileName = dir.name();
       //size_t fileSize = dir.size();
     }
@@ -106,20 +104,20 @@ void FS_init(void)
   ws.onEvent(onWsEvent);
   server.addHandler(&ws);
 
-  events.onConnect([](AsyncEventSourceClient * client) {
+  events.onConnect([](AsyncEventSourceClient *client) {
     client->send("hello!", NULL, millis(), 1000);
   });
   server.addHandler(&events);
 
   server.addHandler(new SPIFFSEditor(SPIFFS, http_username, http_password));
 
-  server.on("/heap", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", get_sys_info());
   });
 
   //server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.htm");
 
-  server.onNotFound([](AsyncWebServerRequest * request) {
+  server.onNotFound([](AsyncWebServerRequest *request) {
     Serial.printf("NOT_FOUND: ");
     if (request->method() == HTTP_GET)
       Serial.printf("GET");
@@ -147,13 +145,13 @@ void FS_init(void)
     int headers = request->headers();
     int i;
     for (i = 0; i < headers; i++) {
-      AsyncWebHeader* h = request->getHeader(i);
+      AsyncWebHeader *h = request->getHeader(i);
       Serial.printf("_HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
     }
 
     int params = request->params();
     for (i = 0; i < params; i++) {
-      AsyncWebParameter* p = request->getParam(i);
+      AsyncWebParameter *p = request->getParam(i);
       if (p->isFile()) {
         Serial.printf("_FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
       } else if (p->isPost()) {
@@ -165,21 +163,20 @@ void FS_init(void)
 
     request->send(404);
   });
-  server.onFileUpload([](AsyncWebServerRequest * request, const String & filename, size_t index, uint8_t *data, size_t len, bool final) {
+  server.onFileUpload([](AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) {
     if (!index)
       Serial.printf("UploadStart: %s\n", filename.c_str());
-    Serial.printf("%s", (const char*)data);
+    Serial.printf("%s", (const char *)data);
     if (final)
       Serial.printf("UploadEnd: %s (%u)\n", filename.c_str(), index + len);
   });
-  server.onRequestBody([](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+  server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
     if (!index)
       Serial.printf("BodyStart: %u\n", total);
-    Serial.printf("%s", (const char*)data);
+    Serial.printf("%s", (const char *)data);
     if (index + len == total)
       Serial.printf("BodyEnd: %u\n", total);
   });
-
 }
 
 bool exists(String path) {
@@ -291,7 +288,7 @@ String get_sys_info() {
 
 void save_profile() {
   File file = SPIFFS.open(get_prf_name(), FILE_WRITE);
-  file.write((byte*) &SamSetup, sizeof(SamSetup));
+  file.write((byte *)&SamSetup, sizeof(SamSetup));
   file.close();
   EEPROM.put(0, SamSetup);
   EEPROM.commit();
@@ -303,14 +300,13 @@ void load_profile() {
   if (SPIFFS.exists(f)) {
     File file = SPIFFS.open(f, FILE_READ);
     file.setTimeout(0);
-    file.read((byte*) &SamSetup, sizeof(SamSetup));
+    file.read((byte *)&SamSetup, sizeof(SamSetup));
     file.close();
   } else
     save_profile();
   EEPROM.put(0, SamSetup);
   EEPROM.commit();
   read_config();
-
 }
 
 String get_prf_name() {
