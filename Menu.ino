8ffc498ada87c7d286b3c6f8ecde914f071e5035
LKosoj <lkosoj@gmail.com>
2022-01-07 16:19:33 +0300

    Update Menu.ino

--- Menu.ino
+++ Menu.ino
@@ -355,7 +355,7 @@ void menu_samovar_start() {
   if (startval == 0) {
 #ifdef USE_MQTT
     SessionDescription.replace(",", ";");
-    SessionDescription.replace("%", "&#37;");
+    SessionDescription.replace("%", "prc");
     MqttSendMsg((String)chipId + "," + SamSetup.TimeZone + "," + vr + "," + get_program(CAPACITY_NUM * 2) + "," + SessionDescription, "st");
 #endif  
     startval = 1;
