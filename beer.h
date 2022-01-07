7b1fb202fcfd8f66dcf2460a0c6ce6cfe1022516
LKosoj <lkosoj@gmail.com>
2022-01-07 16:23:06 +0300

    Update beer.h

--- beer.h
+++ beer.h
@@ -21,7 +21,6 @@ void beer_proc() {
   if (startval == 2000 && !PowerOn) {
 #ifdef USE_MQTT
     SessionDescription.replace(",", ";");
-    SessionDescription.replace("%", "prc");
     MqttSendMsg((String)chipId + "," + SamSetup.TimeZone + "," + vr + "," + get_beer_program() + "," + SessionDescription, "st");
 #endif  
     create_data();  //создаем файл с данными
