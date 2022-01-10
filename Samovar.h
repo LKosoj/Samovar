04075716e957ad4959882c63bdc9bbab32657556
LKosoj <lkosoj@gmail.com>
2022-01-10 12:38:04 +0300

    Update Samovar.h

--- Samovar.h
+++ Samovar.h
@@ -526,7 +526,6 @@ String ofl;                                                     // Openlog filen
 bool mixer_status;                                              // Статус работы мешалки
 int bk_pwm;                                                     // Значение PWM насоса при работе с БК
 uint32_t chipId = 0;                                            // Идентификатор ESP32
-float esp32_temp;                                               // Температура ESP32
 String vr;                                                      // Причина перезагрузки ESP32
 String SessionDescription;                                      // Описание параметров работы в свободном формате для сохранения в облаке
 unsigned long server_heart_beat;                                // Время последнего запроса к вебсерверу
