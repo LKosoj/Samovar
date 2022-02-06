c1afdf632b8d9e97bc13876a6dcbd05a5fbd8729
LKosoj <lkosoj@gmail.com>
2022-02-06 15:41:21 +0300

    docs

---
+++ docs/js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [2,1,4,3],
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,1,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,1,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<НОВОЕ КЛЮЧЕВОЕ СЛОВО>","Переключение между режимами.","WEB-интерфейс","Скорость","Разгон"],
+	DREX_KEYWORD_CHILD_START: [1,5,5,5,5],
+	DREX_KEYWORD_CHILD_END: [5,5,5,5,5]
+	
+};
