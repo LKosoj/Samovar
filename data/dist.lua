8c35a044c0597dfdf5ed8538f435c9788a3517ae
LKosoj <lkosoj@gmail.com>
2023-04-24 00:25:12 +0300

    Update dist.lua

--- data/dist.lua
+++ data/dist.lua
@@ -1,138 +1,73 @@
+--Скрипт для проведения дистилляции с разбиением по трем емкостям.
+--Дистилляция начитается с 0 емкости. Запоминаем температуру в кубе в начале кипения.
+--При повышении Т в кубе на заданную уставку, переключаем сервопривод на заданную позицию.
 
--- УСТАНОВКИ ДЛЯ ДАТЧИКА УРОВНЯ ---------------
-min_bottom_level = 150 -- минимальные показания датчика при достижении жидкостью нижнего контакта
-delta_top_percent = 40 -- минимальная разница (в % от нижнего) между показаниями нижнего и верхнего контактов
-bottom_readings_skip = 4 -- сколько показаний нижнего пропустить, прежде чем записать итог
--- УСТАНОВКИ ДЛЯ ДАТЧИКА ПОТОКА ---------------
-target_volume = 30 -- целевой объём в литрах, который требуется набрать
-flow_factor = 1.0 -- коэффициент корректировки неточности датчика потока 
+--НАЧАЛЬНЫЕ УСТАНОВКИ
+t_delta1 = 4 --первая уставка температуры в кубе, при превышении сменится емкость
+t_delta2 = 4 --вторая уставка температуры в кубе, при превышении сменится емкость
+power_delta = 10 -- на столько процентов снизится текущая мощность, если 0 - не снижаем
+use_temp = 0 --если 1 - то использовать переход на следующую емкость по температуре, если 0 - то по спиртуозности
 
--- ИСПОЛЬЗУЕМЫЕ ДАТЧИКИ --------------
-use_level_sensor = true
-use_flow_sensor = false
+-- ОПРЕДЕЛЕНИЕ ПЕРЕМЕННЫХ
+b_temp = getNumVariable("boil_temp") + 0 --получаем запомненную температуру кипения
+alcohol = getNumVariable("alcohol") + 0 --получаем текущую спиртуозность, посчитанную по температуре куба
+alcohol_s = getNumVariable("alcohol_s") + 0 --получаем спиртуозность на момент закипания куба
+TankTemp = getNumVariable("TankTemp") + 0 --получаем текущую Т куба
+PowerOn = getNumVariable("PowerOn") + 0 --получаем статус подачи мощности
+target_power_volt = getNumVariable("target_power_volt") + 0 --получаем текущую мощность
+capacity_num = getNumVariable("capacity_num") + 0 --получаем текущую емкость
+sg = getObject("sg", "NUMERIC") + 0 -- получаем статус начала работы скрипта
+gb = getObject("gb", "NUMERIC") + 0 -- получаем статус реакции на начало кипения
 
--- ОПРЕДЕЛЕНИЕ ПЕРЕМЕННЫХ ---
-tank_filled = getObject("tank_filled") -- читаем, наполнен ли куб
-pump_started = getObject("pump_started") -- читаем признак включения насоса
-bottom_pin = getObject("bottom_pin", "NUMERIC") + 0 -- читаем сохранённый нижний уровень
-bottom_readings_count = getObject("bottom_readings_count", "NUMERIC") + 0 -- читаем количество уже пропущенных показаний
-start_time = getObject("start_time", "NUMERIC") + 0 -- читаем время начала работы насоса
-last_reading_time = getObject("last_reading_time", "NUMERIC") + 0 -- читаем время последнего измерения потока
-last_reading_flow = getObject("last_reading_flow", "NUMERIC") + 0 -- читаем последнее измерение потока
-total_volume = getObject("total_volume", "NUMERIC") + 0 -- читаем общий уже набранный объём
+local function changeCapacity(num)
+  setCapacity(num) --устанавливаем емкость №num
+  sendMsg("Установлена емкость "..num.."!", -1) --пишем в консоль браузера
+  sendMsg("Установлена емкость "..num.."!", 2) --пишем оператору
+end
 
+-- скрипт начал работу
+if (sg == 0) then
+  setObject("sg", 1)
+  changeCapacity(0)
+  setPower(1)
+  setLuaStatus("Начата дистилляция по Габриелю")
+  sendMsg("Начата дистилляция по Габриелю!", 2) --пишем оператору
+end
 
-
-sensor = analogRead() --читаем аналоговое значение пина 34 (к нему подключён датчик уровня)
-
-local function verifyVolumeTargets () -- проверяем корректность данных для датчика потока
-  if (target_volume == nil or target_volume == 0 or type(target_volume) ~= "number" or  flow_factor == nil or flow_factor == 0 or type(flow_factor) ~= "number") then
-    sendMsg("Ошибка в параметрах объёма!", -1) --отчитываемся в консоль браузера
-    sendMsg("Ошибка в параметрах объёма!", 0) --отправляем сообщение оператору
-    use_flow_sensor = false
+-- среагировали в скрипте на начало кипения
+if (gb == 0 and b_temp > 0) then
+  setObject("gb", 1)
+  sendMsg("Начат отбор в емкость №0!", 2) --пишем оператору
+  --началось кипение - снижаем мощность на power_delta процентов
+  if (PowerOn + 0 == 1 and target_power_volt == 0) then
+    --если режим разгона, в котором не известно текущее напряжение
+    target_power_volt = 220
+  end
+  --если power_delta > 0 меняем целевое напряжение
+  if (power_delta > 0) then
+    target_power_volt = target_power_volt - target_power_volt/100*power_delta
+    setCurrentPower(target_power_volt)
   end
 end
 
-local function startPump()
-  pinMode(4, OUTPUT) --устанавливаем режим пина
-	digitalWrite(4, HIGH) --устанавливаем на 4 ноге высокий уровень => включаем насос
-	setObject("pump_started", "true") --сохраняем признак включения насоса
-	sendMsg("Насос включён", -1) --отчитываемся в консоль браузера
-	sendMsg("Насос включён", 2) --отправляем сообщение оператору
+--Обрабатываем логику переключения емкостей
+if b_temp > 0 then
+setLuaStatus(string.format("Текущая спиртуозность  = %.2f; Т начала кипения = %.2f", alcohol, b_temp))
+  if (use_temp == 1) then
+  --логика по температуре
+    if ((capacity_num + 0 == 0) and ((b_temp + t_delta1) <= TankTemp)) then
+      changeCapacity(1)
+    elseif ((capacity_num + 0 == 1) and ((b_temp + t_delta1 + t_delta2) <= TankTemp)) then
+      changeCapacity(2)
+    end
+  else
+  --логика по спиртуозности
+    if (capacity_num + 0 == 0) and (alcohol <= alcohol_s / 2) then
+      --спирта осталась половина - переключаем на 1 емкость
+      changeCapacity(1)
+    elseif (capacity_num + 0 == 1) and (alcohol <= alcohol_s / 4) then
+      --спирта осталась четверть - переключаем на 2 емкость
+      changeCapacity(2)
+    end
+  end
 end
-
-
-local function stopPump()
-  pinMode(4, OUTPUT) --устанавливаем режим пина
-	digitalWrite(4, LOW) --устанавливаем на 4 ноге низкий уровень => выключаем насос
-	setObject("pump_started", "false") --сохраняем признак выключения насоса
-	sendMsg("Насос выключен", -1) --сообщаем в консоль браузера
-	sendMsg("Насос выключен", 2) --отправляем сообщение оператору
-end
-
-
-
-local function check4level()
-	if bottom_pin > 0 then
-		if sensor > bottom_pin * (1 + delta_top_percent/100) then --скачок при достижении верхнего контакта больше минимальной дельты
-			sendMsg("Датчик уровня: куб заполнен.", -1) --отчитываемся в консоль браузера
-			sendMsg("Датчик уровня: куб заполнен.", 0) --отправляем сообщение оператору
-			return true --куб заполнен
-		end
-	elseif sensor > min_bottom_level then
-		if (bottom_readings_count == bottom_readings_skip) then
-			setObject("bottom_pin", sensor) --сохраняем значение для нижнего контакта
-			sendMsg("bottom_pin: " .. sensor, -1)
-			else
-			setObject("bottom_readings_count", bottom_readings_count + 1) --увеличиваем счётчик пропущенных
-		end
-	end
-	return false	
-end
-
-
-
-
-local function check4volume()
-  current_rate = getNumVariable(WFflowRate)
-  
-end
-
-
-
------------------------------------------
---ACTIONS--------------------------------
------------------------------------------
-
-local function stopFilling ()
-	stopPump()
-  setLuaStatus("Куб заполнен")
-	setObject("bottom_pin", 0)
-	setObject("tank_filled", "true")
-	sendMsg("Done: filling stopped.", -1)
-	sendMsg("Done: filling stopped.", 0)
-end
-
-
-
-local function fillTank ()
-  sendMsg("tank_filled: " .. tank_filled, -1)
-  sendMsg("bottom_pin: " .. bottom_pin, -1)
-  sendMsg("sensor: " .. sensor, -1)
-	if tank_filled ~= "true" then
-	  sendMsg("pump_started: " .. pump_started, -1)
-	  if pump_started ~= "true" then
-			sendMsg("Начинаем заполнение куба...", -1) --отчитываемся в консоль браузера
-			sendMsg("Начинаем заполнение куба...", 2) --отправляем сообщение оператору
-            setLuaStatus("Заполнение куба")
-            startPump()
-		else
-      if use_level_sensor and check4level() then stopFilling() end
-      if use_flow_sensor and check4volume() then stopFilling() end
-		end
-  else 
-    sendMsg("NOTHING 2 DO: tank_filled: " .. tank_filled, -1)
-    sendMsg("NOTHING 2 DO: tank_filled: " .. tank_filled, 0)
-	end
-end
-
-
-
-local function resetFilling ()
-	sendMsg("tank_filled: " .. tank_filled, 0)
-	setObject("tank_filled", "false")
-  setObject("bottom_pin", 0)
-	sendMsg("Done: filling reset.", -1)
-	sendMsg("Done: filling reset.", 0)
-end
-
-
-
---RUN--------------------------------
-
---resetFilling ()
- fillTank()
--- stopFilling()
-
-
-
