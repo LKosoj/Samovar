f7c798ab6dbeff254160dd28bb76da9053a76eb9
LKosoj <lkosoj@gmail.com>
2023-04-24 00:25:21 +0300

    Delete dist1.lua

--- data/dist1.lua
+++
@@ -1,7 +0,0 @@
-target_temp = 94 --температура, с которой начнется укрепление при дистилляции - откроется клапан воды для охлаждения.
-
-if (PowerOn == 1 and valve_status == 1 and TankTemp < target_temp) then
-  openValve(0)
-else if (PowerOn == 1 and valve_status == 0 and TankTemp >= target_temp) then
-  openValve(1)
-end
