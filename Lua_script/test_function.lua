a5901ecf6a640d6a0cec8a7074b835cb6829af7b
LKosoj <lkosoj@gmail.com>
2023-05-07 23:48:28 +0300

    Create test_function.lua

---
+++ Lua_script/test_function.lua
@@ -0,0 +1,27 @@
+local function getNumVariable(var)
+  return 0
+end
+
+local function getObject(val, type)
+  return 0
+end
+
+local function setCapacity(num)
+  
+end
+
+local function setObject(var, val)
+  
+end
+
+local function sendMsg(msg, type)
+   print (msg)
+end
+
+local function setPower(mode)
+
+end
+
+local function setLuaStatus(status)
+   sendMsg(status, 1)
+end
