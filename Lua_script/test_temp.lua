a5428cf1f9876942b76a81fd4b1a844592aa2386
LKosoj <lkosoj@gmail.com>
2023-06-26 00:30:17 +0300

    Create test_temp.lua

---
+++ Lua_script/test_temp.lua
@@ -0,0 +1,36 @@
+setNumVariable("WFpulseCount",20)
+
+pos = 1
+
+if pos == 1 then
+  setNumVariable("TankTemp",50)
+  setNumVariable("WaterTemp",20)
+
+elseif pos == 2 then
+  setNumVariable("TankTemp",78)
+  setNumVariable("WaterTemp",23)
+
+elseif pos == 3 then
+  setNumVariable("TankTemp",82)
+  setNumVariable("WaterTemp",24)
+
+elseif pos == 4 then
+  setNumVariable("TankTemp",83)
+  setNumVariable("WaterTemp",25)
+
+elseif pos == 5 then
+  setNumVariable("TankTemp",84)
+  setNumVariable("WaterTemp",19)
+
+elseif pos == 6 then
+  setNumVariable("TankTemp",85)
+  setNumVariable("WaterTemp",27)
+
+elseif pos == 7 then
+  setNumVariable("TankTemp",86)
+  setNumVariable("WaterTemp",49)
+
+elseif pos == 8 then
+  setNumVariable("TankTemp",87)
+  setNumVariable("WaterTemp",49)
+end
