43a348d90c6b07d0779162a1862e3cae68203bd5
LKosoj <lkosoj@gmail.com>
2023-06-25 22:04:03 +0300

    Update beer_mash.lua

--- Lua_script/beer_mash.lua
+++ Lua_script/beer_mash.lua
@@ -2,26 +2,21 @@
 
 --НАЧАЛЬНЫЕ УСТАНОВКИ
 T = 19 -- температура, ниже которой скрипт не работает
-Time = 120 -- время задержки скрипта в секундах (чаще реле включаться не будет)
 
 -- ОПРЕДЕЛЕНИЕ ПЕРЕМЕННЫХ
 TankTemp = getNumVariable("TankTemp") + 0
 ACPTemp = getNumVariable("ACPTemp") + 0
 ValveStatus = getNumVariable("valve_status") + 0
-Timer = getTimer(1) + 0
 
-if TankTemp < T and ValveStatus == 1 and Timer == 0 then
+if TankTemp < T and ValveStatus == 1 then
    openValve(0)
-   setTimer(1, Time)
 else
-   if ACPTemp + 0.5 < TankTemp and ValveStatus == 0 and Timer == 0 then
+   if ACPTemp + 0.5 < TankTemp and ValveStatus == 0 then
       openValve(1)
-      setTimer(1, Time)
    end
 
-   if ACPTemp - 0.5 > TankTemp and ValveStatus == 1 and Timer == 0 then
+   if ACPTemp - 0.5 > TankTemp and ValveStatus == 1 then
       openValve(0)
-      setTimer(1, Time)
    end
 
 end
