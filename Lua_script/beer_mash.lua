d6cfc4d847bec8ebcffd9878118329e3b4ab0655
LKosoj <lkosoj@gmail.com>
2023-07-10 01:16:10 +0300

    Update beer_mash.lua

--- Lua_script/beer_mash.lua
+++ Lua_script/beer_mash.lua
@@ -2,10 +2,10 @@
 
 --НАЧАЛЬНЫЕ УСТАНОВКИ
 Tmin = 19 --минимальная температура куба,  ниже которой скрипт не работает
-dACP = 0.7 -- дельта датчика ТСА
-dWater = 0.5 -- дельта датчика воды.
-Time1 = 3600 -- время задержки скрипта в секундах
-Time2 = 300 --  Время работы клапана в секундах
+dACP = 2 -- дельта датчика ТСА
+dWater = 1 -- дельта датчика воды.
+Time1 = 10 -- время задержки скрипта в секундах
+Time2 = 3 --  Время работы клапана в секундах
 
 -- ОПРЕДЕЛЕНИЕ ПЕРЕМЕННЫХ
 TankTemp = getNumVariable("TankTemp") + 0
@@ -20,29 +20,36 @@ if Timer1 == 0 then
       openValve(0)
       setTimer(1, Time1)
   else
+  if TankTemp < ACPTemp + dACP and ValveStatus == 1 and Timer1 == 0 then
+      openValve(0)
+      setTimer(1, Time1)
+  else
   if ACPTemp + dACP < TankTemp and ValveStatus == 0 and Timer2 == 0 then
          openValve(1)
          setTimer(2, Time2)
   else
-  if WaterTemp + dWater < TankTemp and ValveStatus == 0 and Timer2 == 0 then
+  if WaterTemp + dWater < TankTemp and ValveStatus == 0 and ACPTemp + dACP < TankTemp and Timer2 == 0 then
          openValve(1)
          setTimer(2, Time2)
       elseif WaterTemp + dWater >= TankTemp and ValveStatus == 1 and Timer2 == 0 then
          openValve(0)
-         setTimer(2, Time2)
+         setTimer(2, Time1)
+         end
       end
   end
+end
 end
 
 ValveStatus = getNumVariable("valve_status") + 0
 
-status = string.format("ACPT = %.2f; TankT = %.2f; Клапан %.0f", 
-ACPTemp, TankTemp, ValveStatus)
+status = string.format("ACPT = %.2f; TankT = %.2f; WaterTemp = %.2f; Клапан %.0f", ACPTemp, TankTemp, WaterTemp, ValveStatus)
 
 setLuaStatus(status)
 
 --проверяем признак завершения работы скрипта, если он установлен, то завершаем работу
 SetScriptOff = getNumVariable("SetScriptOff") + 0
+
 if SetScriptOff == 1 then
+  setLuaStatus("Скрипт остановлен")
   openValve(0)
 end
