4f8fde9de28229b091905772491d8255f5069414
LKosoj <lkosoj@gmail.com>
2024-02-01 00:36:00 +0300

    Revert "berr.h"
    This reverts commit 8e2a4dce2793c54f125f95923ba9360c8751e88c.

--- beer.h
+++ beer.h
@@ -41,8 +41,6 @@ void run_beer_program(byte num) {
   if (startval == 2000) startval = 2001;
   ProgramNum = num;
   begintime = 0;
-  msgfl = true;
-
   if (program[ProgramNum].WType == "A") {
     StartAutoTune();
   }
@@ -539,15 +537,15 @@ void set_mixer(bool On) {
 }
 
 //Крутим шаговым двигателем на заданное количество шагов
-void HopStepperStep() {
-  stopService();
-  stepper.brake();
-  stepper.disable();
-  stepper.setMaxSpeed(200); //скорость движения шагового двигателя
-  stepper.setSpeed(200);    //скорость движения шагового двигателя, должна быть равна предыдущей
-  TargetStepps = 360 / 1.8 * 16 / 20;  //16 - множитель на драйвере двигателя. 20 - количество отверстий по целому кругу (если бы они занимали всю окружность)
-  stepper.setCurrent(0);
-  stepper.setTarget(TargetStepps);
-  stepper.enable();
-  startService();
+void HopStepperStep(){
+    stopService();
+    stepper.brake();
+    stepper.disable();
+    stepper.setMaxSpeed(200); //скорость движения шагового двигателя
+    stepper.setSpeed(200);    //скорость движения шагового двигателя, должна быть равна предыдущей
+    TargetStepps = 360 / 1.8 * 16 / 20;  //16 - множитель на драйвере двигателя. 20 - количество отверстий по целому кругу (если бы они занимали всю окружность)
+    stepper.setCurrent(0);
+    stepper.setTarget(TargetStepps);
+    stepper.enable();
+    startService();    
 }
