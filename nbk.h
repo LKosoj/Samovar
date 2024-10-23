5c0bb3eaac73eab1905cc3179c8b546b3b0bf748
LKosoj <lkosoj@gmail.com>
2024-10-24 00:31:21 +0300

    Update nbk.h

--- nbk.h
+++ nbk.h
@@ -14,6 +14,9 @@ void set_water_temp(float duty);
 String getValue(String data, char separator, int index);
 void run_nbk_program(uint8_t num);
 bool set_stepper_target(uint16_t spd, uint8_t direction, uint32_t target);
+float i2c_get_liguid_rate_by_step(int StepperSpeed);
+float i2c_get_speed_from_rate(float volume_per_hour);
+float i2c_get_liquid_volume();
 
 // Описание алгоритма работы
 // 1) Прогрев на мощности 3000 Вт с подачей браги 3 л/ч до температуры пара 85 гр.Ц.
