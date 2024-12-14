a2db694042c9fe40a452d7db99f1f24b683e94f8
LKosoj <lkosoj@gmail.com>
2024-12-14 21:06:29 +0300

    Update user_config_override.h

--- user_config_override.h
+++ user_config_override.h
@@ -15,5 +15,7 @@
 
 //#define USE_EXPANDER 0x20                    //использовать расширитель портов PCF8575
 //#define USE_ANALOG_EXPANDER 0x48             //использовать расширитель аналоговых портов PCF8591
+//#define USE_TELEGRAM                          //отправлять уведомления в телеграм, для этого в настройках необходимо указать токен бота и идентификатор пользователя. 
+//#define SAMOVAR_USE_BLYNK                    //использовать Blynk в проекте
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
