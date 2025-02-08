ccf2e5690fa5becd9219307bd18283cc6ade760d
LKosoj <lkosoj@gmail.com>
2025-02-08 17:59:38 +0300

    ESP Async Web Server update

--- libraries/ESP_Async_WebServer/examples/CaptivePortal/CaptivePortal.ino
+++ libraries/ESP_Async_WebServer/examples/CaptivePortal/CaptivePortal.ino
@@ -1,36 +1,39 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2016-2025 Hristo Gochkov, Mathieu Carbou, Emil Muratov
+
 #include <DNSServer.h>
 #ifdef ESP32
-  #include <AsyncTCP.h>
-  #include <WiFi.h>
+#include <AsyncTCP.h>
+#include <WiFi.h>
 #elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
 #elif defined(TARGET_RP2040)
-  #include <WebServer.h>
-  #include <WiFi.h>
+#include <WebServer.h>
+#include <WiFi.h>
 #endif
 #include "ESPAsyncWebServer.h"
 
-DNSServer dnsServer;
-AsyncWebServer server(80);
+static DNSServer dnsServer;
+static AsyncWebServer server(80);
 
 class CaptiveRequestHandler : public AsyncWebHandler {
-  public:
-    bool canHandle(__unused AsyncWebServerRequest* request) const override {
-      return true;
-    }
+public:
+  bool canHandle(__unused AsyncWebServerRequest *request) const override {
+    return true;
+  }
 
-    void handleRequest(AsyncWebServerRequest* request) {
-      AsyncResponseStream* response = request->beginResponseStream("text/html");
-      response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
-      response->print("<p>This is our captive portal front page.</p>");
-      response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
+  void handleRequest(AsyncWebServerRequest *request) {
+    AsyncResponseStream *response = request->beginResponseStream("text/html");
+    response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
+    response->print("<p>This is our captive portal front page.</p>");
+    response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
 #ifndef CONFIG_IDF_TARGET_ESP32H2
-      response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
+    response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
 #endif
-      response->print("</body></html>");
-      request->send(response);
-    }
+    response->print("</body></html>");
+    request->send(response);
+  }
 };
 
 void setup() {
@@ -41,14 +44,13 @@ void setup() {
 #ifndef CONFIG_IDF_TARGET_ESP32H2
   if (!WiFi.softAP("esp-captive")) {
     Serial.println("Soft AP creation failed.");
-    while (1)
-      ;
+    while (1);
   }
 
   dnsServer.start(53, "*", WiFi.softAPIP());
 #endif
 
-  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER); // only when requested from AP
+  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);  // only when requested from AP
   // more handlers...
   server.begin();
 }
--- libraries/ESP_Async_WebServer/examples/Filters/Filters.ino
+++ libraries/ESP_Async_WebServer/examples/Filters/Filters.ino
@@ -1,38 +1,43 @@
-// Reproduced issue https://github.com/ESP32Async/ESPAsyncWebServer/issues/26
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2016-2025 Hristo Gochkov, Mathieu Carbou, Emil Muratov
+
+//
+// Shows how to use setFilter to route requests to different handlers based on WiFi mode
+//
 
 #include <DNSServer.h>
 #ifdef ESP32
-  #include <AsyncTCP.h>
-  #include <WiFi.h>
+#include <AsyncTCP.h>
+#include <WiFi.h>
 #elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
 #elif defined(TARGET_RP2040)
-  #include <WebServer.h>
-  #include <WiFi.h>
+#include <WebServer.h>
+#include <WiFi.h>
 #endif
 #include "ESPAsyncWebServer.h"
 
-DNSServer dnsServer;
-AsyncWebServer server(80);
+static DNSServer dnsServer;
+static AsyncWebServer server(80);
 
 class CaptiveRequestHandler : public AsyncWebHandler {
-  public:
-    bool canHandle(__unused AsyncWebServerRequest* request) const override {
-      return true;
-    }
+public:
+  bool canHandle(__unused AsyncWebServerRequest *request) const override {
+    return true;
+  }
 
-    void handleRequest(AsyncWebServerRequest* request) override {
-      AsyncResponseStream* response = request->beginResponseStream("text/html");
-      response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
-      response->print("<p>This is out captive portal front page.</p>");
-      response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
+  void handleRequest(AsyncWebServerRequest *request) override {
+    AsyncResponseStream *response = request->beginResponseStream("text/html");
+    response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
+    response->print("<p>This is out captive portal front page.</p>");
+    response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
 #ifndef CONFIG_IDF_TARGET_ESP32H2
-      response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
+    response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
 #endif
-      response->print("</body></html>");
-      request->send(response);
-    }
+    response->print("</body></html>");
+    request->send(response);
+  }
 };
 
 bool hit1 = false;
@@ -42,49 +47,55 @@ void setup() {
   Serial.begin(115200);
 
   server
-    .on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-      Serial.println("Captive portal request...");
+    .on(
+      "/", HTTP_GET,
+      [](AsyncWebServerRequest *request) {
+        Serial.println("Captive portal request...");
 #ifndef CONFIG_IDF_TARGET_ESP32H2
-      Serial.println("WiFi.localIP(): " + WiFi.localIP().toString());
+        Serial.println("WiFi.localIP(): " + WiFi.localIP().toString());
 #endif
-      Serial.println("request->client()->localIP(): " + request->client()->localIP().toString());
+        Serial.println("request->client()->localIP(): " + request->client()->localIP().toString());
 #if ESP_IDF_VERSION_MAJOR >= 5
-  #ifndef CONFIG_IDF_TARGET_ESP32H2
-      Serial.println("WiFi.type(): " + String((int)WiFi.localIP().type()));
-  #endif
-      Serial.println("request->client()->type(): " + String((int)request->client()->localIP().type()));
+#ifndef CONFIG_IDF_TARGET_ESP32H2
+        Serial.println("WiFi.type(): " + String((int)WiFi.localIP().type()));
+#endif
+        Serial.println("request->client()->type(): " + String((int)request->client()->localIP().type()));
 #endif
 #ifndef CONFIG_IDF_TARGET_ESP32H2
-      Serial.println(WiFi.localIP() == request->client()->localIP() ? "should be: ON_STA_FILTER" : "should be: ON_AP_FILTER");
-      Serial.println(WiFi.localIP() == request->client()->localIP());
-      Serial.println(WiFi.localIP().toString() == request->client()->localIP().toString());
+        Serial.println(WiFi.localIP() == request->client()->localIP() ? "should be: ON_STA_FILTER" : "should be: ON_AP_FILTER");
+        Serial.println(WiFi.localIP() == request->client()->localIP());
+        Serial.println(WiFi.localIP().toString() == request->client()->localIP().toString());
 #endif
-      request->send(200, "text/plain", "This is the captive portal");
-      hit1 = true;
-    })
+        request->send(200, "text/plain", "This is the captive portal");
+        hit1 = true;
+      }
+    )
     .setFilter(ON_AP_FILTER);
 
   server
-    .on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-      Serial.println("Website request...");
+    .on(
+      "/", HTTP_GET,
+      [](AsyncWebServerRequest *request) {
+        Serial.println("Website request...");
 #ifndef CONFIG_IDF_TARGET_ESP32H2
-      Serial.println("WiFi.localIP(): " + WiFi.localIP().toString());
+        Serial.println("WiFi.localIP(): " + WiFi.localIP().toString());
 #endif
-      Serial.println("request->client()->localIP(): " + request->client()->localIP().toString());
+        Serial.println("request->client()->localIP(): " + request->client()->localIP().toString());
 #if ESP_IDF_VERSION_MAJOR >= 5
-  #ifndef CONFIG_IDF_TARGET_ESP32H2
-      Serial.println("WiFi.type(): " + String((int)WiFi.localIP().type()));
-  #endif
-      Serial.println("request->client()->type(): " + String((int)request->client()->localIP().type()));
+#ifndef CONFIG_IDF_TARGET_ESP32H2
+        Serial.println("WiFi.type(): " + String((int)WiFi.localIP().type()));
+#endif
+        Serial.println("request->client()->type(): " + String((int)request->client()->localIP().type()));
 #endif
 #ifndef CONFIG_IDF_TARGET_ESP32H2
-      Serial.println(WiFi.localIP() == request->client()->localIP() ? "should be: ON_STA_FILTER" : "should be: ON_AP_FILTER");
-      Serial.println(WiFi.localIP() == request->client()->localIP());
-      Serial.println(WiFi.localIP().toString() == request->client()->localIP().toString());
+        Serial.println(WiFi.localIP() == request->client()->localIP() ? "should be: ON_STA_FILTER" : "should be: ON_AP_FILTER");
+        Serial.println(WiFi.localIP() == request->client()->localIP());
+        Serial.println(WiFi.localIP().toString() == request->client()->localIP().toString());
 #endif
-      request->send(200, "text/plain", "This is the website");
-      hit2 = true;
-    })
+        request->send(200, "text/plain", "This is the website");
+        hit2 = true;
+      }
+    )
     .setFilter(ON_STA_FILTER);
 
   // assert(WiFi.softAP("esp-captive-portal"));
@@ -121,4 +132,5 @@ void setup() {
 }
 
 void loop() {
+  delay(100);
 }
