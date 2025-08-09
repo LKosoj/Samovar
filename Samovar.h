37c9f8bb33171a5219abd1bb0d13f86d438b9962
LKosoj <lkosoj@gmail.com>
2025-08-09 17:44:30 +0300

    Update Samovar.h

--- Samovar.h
+++ Samovar.h
@@ -8,7 +8,7 @@
 #error This code is designed to run on ESP32 platform, not Arduino nor ESP8266! Please check your Tools->Board setting.
 #endif
 
-#define SAMOVAR_VERSION F("6.24")
+#define SAMOVAR_VERSION F("6.25")
 
 //#define __SAMOVAR_DEBUG
 
